516dbf928b7de14292e0275458c5175ffb4913ee
juhikumarimodi6 <71226446+juhikumarimodi6@users.noreply.github.com>
2023-05-18 00:27:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -12,35 +12,6 @@ function App() {
   const [editList, setEditList] = React.useState(false);
   const [editListIndex, setEditListIndex] = React.useState();
 
-  //To drag and drop, setting priority of task.
-  let itemDragged, listArr, dropIndex;
-  let markdoneArr, marked;
-
-  const handleDragStart = (event, index) => {
-
-    itemDragged = lists[index];  // extract drag item from list
-    listArr =[...lists];        // create duplicate List array and perform operation into it. 
-    listArr.splice(index,1);    // remove element from array at particular index.
-
-    marked=doneCheck[index];      //similarly for the marked/notMarked array
-    markdoneArr = [...doneCheck];
-    markdoneArr.splice(index,1);
-  }
-
-  //Record the drop point index
-  const handleDragEnter = (event, id) => {
-    dropIndex=id;
-  }
-
-  //place list item and assigned marked symbol at drop Point
-  const handleDragEnd = () => {
-    console.log(dropIndex)
-    listArr.splice(dropIndex, 0 , itemDragged);   //insert item in an array at particular index
-    setLists([...listArr])                        // overwrite list array with new list array
-    markdoneArr.splice(dropIndex, 0 , marked);    //same for marked symbol array
-    setDoneCheck([...markdoneArr])
-  }
-
   return (
     <div className='app-container'>
       <Header />
@@ -65,9 +36,6 @@ function App() {
                     setDoneCheck = {setDoneCheck}
                     setEditList = {setEditList}
                     setEditListIndex = {setEditListIndex}
-                    handleDragStart = {handleDragStart}
-                    handleDragEnter = {handleDragEnter}
-                    handleDragEnd = {handleDragEnd}
                   />
         })
       }
--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -4,7 +4,34 @@ import List from "./List";
 import Done from "./Done";
 import '../App.css'
 
-const ListContainer = ({list,index,lists,doneCheck,setInput,setLists,setDoneCheck,setEditList,setEditListIndex,handleDragStart, handleDragEnter,handleDragEnd}) => {
+  //To drag and drop, setting priority of task.
+  let itemDragged, listArr, dropIndex;
+  let markdoneArr, marked;
+
+const ListContainer = ({list,index,lists,doneCheck,setInput,setLists,setDoneCheck,setEditList,setEditListIndex}) => {
+  
+  const handleDragStart = (event, index) => {
+    itemDragged = lists[index];  // extract drag item from list
+    listArr =[...lists];        // create duplicate List array and perform operation into it. 
+    listArr.splice(index,1);    // remove element from array at particular index.
+
+    marked=doneCheck[index];      //similarly for the marked/notMarked array
+    markdoneArr = [...doneCheck];
+    markdoneArr.splice(index,1);
+  }
+
+  //Record the drop point index
+  const handleDragEnter = (event, id) => {
+    dropIndex=id;
+  }
+
+  //place list item and assigned marked symbol at drop Point
+  const handleDragEnd = () => {
+    listArr.splice(dropIndex, 0 , itemDragged);   //insert item in an array at particular index
+    setLists([...listArr])                        // overwrite list array with new list array
+    markdoneArr.splice(dropIndex, 0 , marked);    //same for marked symbol array
+    setDoneCheck([...markdoneArr])
+  }
 
     return <div 
                 className='list-container' 
@@ -36,4 +63,3 @@ const ListContainer = ({list,index,lists,doneCheck,setInput,setLists,setDoneChec
 }
 
 export default ListContainer;
-// 
